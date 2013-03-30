4012296f8d047e72472204e8263cccf5b8eaf534
David Dennis <daviddennis@nat-tvwna-inside-visitornet101-b-3786.princeton.org>
2013-03-30 09:25:03 -0400

    _

--- website/website/settings.py
+++ website/website/settings.py
@@ -19,7 +19,7 @@ DATABASES = {
         'NAME': 'website',                      # Or path to database file if using sqlite3.
         'USER': 'postgres',                      # Not used with sqlite3.
         'PASSWORD': 'postgres',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
